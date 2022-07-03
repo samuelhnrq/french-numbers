980d750929f65b4b05742dda96fb5b8e3d426000
--global <--global>
2022-07-03 11:21:42 +0200

    fix: replace placeholder labels

--- components/index/NumberInput.tsx
+++ components/index/NumberInput.tsx
@@ -5,15 +5,18 @@ import { useStore } from "../../lib/store";
 function NumberInput() {
   const [text, setText] = useState("");
   const updateNumber = useStore((x) => x.updateNumber);
-  const newInput = useCallback((ev: React.KeyboardEvent<HTMLInputElement>) => {
-    const element = ev.target as HTMLInputElement;
-    setText(element.value);
-    updateNumber(element.value);
-  }, []);
+  const newInput = useCallback(
+    (ev: React.KeyboardEvent<HTMLInputElement>) => {
+      const element = ev.target as HTMLInputElement;
+      setText(element.value);
+      updateNumber(element.value);
+    },
+    [updateNumber]
+  );
   return (
     <>
       <TextField
-        label="Hello world"
+        label="Cardinal number"
         variant="filled"
         value={text}
         onInput={newInput}
